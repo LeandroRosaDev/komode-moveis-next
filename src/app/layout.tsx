d896c5e18eedd71d034d6057432eb9a29ef972d7
pecinallib <pecinallipoker@gmail.com>
2024-06-08 10:27:33 -0300

    feat: fonte roboto adicionado e pesos novos de fontes tbm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Menu from "@/componentes/menu/Menu";
-import FooterPage from "./footer/page";
-import ScrollToTopButton from "@/componentes/scrolltotop/ScrollToTopButton";
-import WhatsappButton from "@/componentes/whatsapp/WhatsappButton";
-import { Montserrat } from "next/font/google";
+import type { Metadata } from 'next';
+import './globals.css';
+import Menu from '@/componentes/menu/Menu';
+import FooterPage from './footer/page';
+import ScrollToTopButton from '@/componentes/scrolltotop/ScrollToTopButton';
+import WhatsappButton from '@/componentes/whatsapp/WhatsappButton';
+import { Montserrat, Roboto } from 'next/font/google';
 
 export const metadata: Metadata = {
-  title: "Komode Móveis e Decorados",
-  description: "Melhor site de Móveis e Estofados do Brasil",
+  title: 'Komode Móveis e Decorados',
+  description: 'Melhor site de Móveis e Estofados do Brasil',
 };
 const montserrat = Montserrat({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
+  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const roboto = Roboto({
+  weight: ['400', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export default function RootLayout({
@@ -22,7 +28,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={montserrat.className}>
+    <html
+      lang="pt-BR"
+      className={`${montserrat.className} ${roboto.className}`}
+    >
       <body>
         <Menu />
         {children}
