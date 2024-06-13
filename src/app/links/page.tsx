ea8b6c18bf11d8e7a14aad8689c184d3a3524c29
LeandroRosaDev <LeandroRosaDev>
2024-06-13 18:32:39 -0300

    fix: adicionando caminhos corretamente

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -12,9 +12,9 @@ export default function SobrePage() {
         height={100}
       />
       <div className={style.bg2}>
-        <Link
+        <a
           target="blank"
-          href="https://www.whatsapp.com/catalog/5521978991540/?app_absent=0"
+          href="https://www.facebook.com/komodemoveisedecorados"
         >
           <Image
             src="/assets/menu-social-logo-1.png"
@@ -22,20 +22,17 @@ export default function SobrePage() {
             width={30}
             height={30}
           />
-        </Link>
-        <Link
-          target="blank"
-          href="https://www.whatsapp.com/catalog/5521978991540/?app_absent=0"
-        >
+        </a>
+        <a target="blank" href="https://www.instagram.com/komodemd/">
           <Image
             src="/assets/menu-social-logo-3.png"
             alt="Logotipo"
             width={30}
             height={30}
           />
-        </Link>
+        </a>
       </div>
-      <Link
+      <a
         className={style.card}
         target="blank"
         href="https://wa.me/c/5521978991540"
@@ -47,8 +44,8 @@ export default function SobrePage() {
           height={40}
         />
         Converse com a nossa equipe de Vendas
-      </Link>
-      <Link
+      </a>
+      <a
         className={style.card}
         target="blank"
         href="https://www.whatsapp.com/catalog/5521978991540/?app_absent=0"
@@ -60,7 +57,7 @@ export default function SobrePage() {
           height={40}
         />
         Confira nosso catálogo de retráteis
-      </Link>
+      </a>
     </main>
   );
 }
